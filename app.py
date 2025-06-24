13c159fe1fef06a348e37fa40111447f0d00f5e7
bryanlzj <bryanlee828@gmail.com>
2025-06-24 13:09:47 +0800

    Change port to 0.0.0.0

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 from scraper import scrape_all  # import the new function
 
@@ -11,4 +13,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
